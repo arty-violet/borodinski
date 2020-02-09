0995b28645eb9772651a15cdd7a94c232b6efbf0
Arty Soule <artysoule@gmail.com>
2020-02-10 02:23:37 +0300

    корректировка отображения карты и работу кнопки фильтров

--- blocks/main-content/map/map.js
+++ blocks/main-content/map/map.js
@@ -7,13 +7,13 @@ var overlay = document.querySelector(".modal__overlay");
 
 mapLink.addEventListener("click", function (evt) {
   evt.preventDefault();
-  mapPopup.classList.add("modal--show");
+  mapPopup.classList.add("modal--animation-show");
   overlay.classList.add("modal__overlay--show");
 });
 
 mapClose.addEventListener("click", function (evt) {
   evt.preventDefault();
-  mapPopup.classList.remove("modal--show");
+  mapPopup.classList.remove("modal--animation-show");
   overlay.classList.remove("modal__overlay--show");
 });
 
@@ -21,8 +21,8 @@ window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
     evt.preventDefault();
     
-    if (mapPopup.classList.contains("modal--show")) {
-      mapPopup.classList.remove("modal--show");
+    if (mapPopup.classList.contains("modal--animation-show")) {
+      mapPopup.classList.remove("modal--animation-show");
       overlay.classList.remove("modal__overlay--show");
     }
   }
---
+++ js/filter.js
@@ -0,0 +1,12 @@
+
+var filterButton = document.querySelector(".filter__button");
+var filterForm = document.querySelector(".filter__form");
+
+
+filterButton.addEventListener("click", function (evt) {
+  evt.preventDefault();
+});
+
+filterForm.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+});
